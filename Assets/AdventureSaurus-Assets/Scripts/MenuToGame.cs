f03b2e7b8aa15cddc9829a5f36e1e2efdd7f5d25
Melbyj1125 <60886701+Melbyj1125@users.noreply.github.com>
2020-03-02 11:00:51 -0600

    Made a new Scene
    Scene transition file is commented out because I don't understand how to do it correctly yet gonna look into it later.

---
+++ Assets/AdventureSaurus-Assets/Scripts/MenuToGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuToGame : MonoBehaviour
+{
+    /*
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    private void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("\\Assets\\AdventureSaurus-Assets\\Scripts");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+
+
+
+
+   public void sceneToMenu()
+    {
+        Debug.Log("Scene2 loading: " + scenePaths[0]);
+        SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+    }
+    */
+}
