865d2ba121d3f483790fadfee622e080d9a62751
Melbyj1125 <60886701+Melbyj1125@users.noreply.github.com>
2020-03-09 08:12:31 -0500

    GameOver Exists now
    if you die you go to a gameover screen

---
+++ Assets/AdventureSaurus-Assets/Scripts/TransitionScripts/DeathCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DeathCheck : MonoBehaviour
+{
+    public Transform player1;
+    public Transform player2;
+    public Transform player3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player1==null && player2==null && player3==null)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/AdventureSaurus-Assets/Scripts/TransitionScripts/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    public void GameOverToMenu(string menu)
+    {
+        SceneManager.LoadScene(menu);
+    }
+    public void GameOverToCheckpoint(string Checkpoint)
+    {
+        SceneManager.LoadScene(Checkpoint);
+    }
+}
