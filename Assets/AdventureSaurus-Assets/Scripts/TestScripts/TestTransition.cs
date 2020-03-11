756ac022deb8b3382188412e359df50900306cac
Melbyj1125 <60886701+Melbyj1125@users.noreply.github.com>
2020-03-11 10:32:14 -0500

    Was testing how data will save
    made some scenes to test how character data will be held through scenes

---
+++ Assets/AdventureSaurus-Assets/Scripts/TestScripts/TestTransition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TestTransition : MonoBehaviour
+{
+    public void MenuToScene(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
